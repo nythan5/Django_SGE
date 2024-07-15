a9a0c34214088128d6c474076d7ae03163d9fc64
nythan5 <g.n.d2014@gmail.com>
2024-07-15 17:23:46 -0300

    Implementando filtros | BRANDS

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import ListView
+from .models import Brand
 
-# Create your views here.
+
+class BrandListView(ListView):
+    model = Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            qs = qs.filter(name__icontains=name)
+
+        return qs
--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.shortcuts import render
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('brands/', include('brands.urls')),
 
 
 ]
